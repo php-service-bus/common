5ab8e405ddfb5594f3dddd22caa7257ad0bf3bf9
desperado <desperado@minsk-info.ru>
2019-01-22 12:39:31 +0300

    add context and delivery options interface

---
+++ src/Context/ServiceBusContext.php
@@ -0,0 +1,100 @@
+<?php
+
+/**
+ * PHP Service Bus (publish-subscribe pattern implementation) common component
+ *
+ * @author  Maksim Masiukevich <dev@async-php.com>
+ * @license MIT
+ * @license https://opensource.org/licenses/MIT
+ */
+
+declare(strict_types = 1);
+
+namespace ServiceBus\Common\Context;
+
+use Amp\Promise;
+use Psr\Log\LogLevel;
+use ServiceBus\Common\Endpoint\DeliveryOptions;
+use ServiceBus\Common\Messages\Message;
+
+/**
+ * Base context interface
+ */
+interface ServiceBusContext
+{
+    /**
+     * Is the received message correct?
+     * If validation is not enabled in the handler parameters, it always returns true
+     *
+     * @return bool
+     */
+    public function isValid(): bool;
+
+    /**
+     * If received message is incorrect, returns a list of violations
+     *
+     * [
+     *    'propertyPath' => [
+     *        0 => 'some message',
+     *        ....
+     *    ]
+     * ]
+     *
+     * @return array<string, array<int, string>>
+     */
+    public function violations(): array;
+
+    /**
+     * Enqueue message
+     *
+     * @param Message              $message
+     * @param DeliveryOptions|null $deliveryOptions
+     *
+     * @return Promise It does not return any result
+     */
+    public function delivery(Message $message, ?DeliveryOptions $deliveryOptions = null): Promise;
+
+    /**
+     * Log message with context details
+     *
+     * @param string $logMessage
+     * @param array  $extra
+     * @param string $level
+     *
+     * @return void
+     */
+    public function logContextMessage(
+        string $logMessage,
+        array $extra = [],
+        string $level = LogLevel::INFO
+    ): void;
+
+    /**
+     * Log Throwable in execution context
+     *
+     * @param \Throwable $throwable
+     * @param array      $extra
+     * @param string     $level
+     *
+     * @return void
+     */
+    public function logContextThrowable(
+        \Throwable $throwable,
+        string $level = LogLevel::ERROR,
+        array $extra = []
+    ): void;
+
+    /**
+     * Receive incoming operation id
+     *
+     * @return string
+     */
+    public function operationId(): string;
+
+    /**
+     * Receive trace message id
+     *
+     * @return string
+     */
+    public function traceId(): string;
+}
---
+++ src/Endpoint/DeliveryOptions.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * PHP Service Bus (publish-subscribe pattern implementation) common component
+ *
+ * @author  Maksim Masiukevich <dev@async-php.com>
+ * @license MIT
+ * @license https://opensource.org/licenses/MIT
+ */
+
+declare(strict_types = 1);
+
+namespace ServiceBus\Common\Endpoint;
+
+/**
+ *
+ */
+interface DeliveryOptions
+{
+
+}
