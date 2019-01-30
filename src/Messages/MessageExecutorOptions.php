dbe2d12564f55a65f80b79ef87f2526bab8d119f
desperado <desperado@minsk-info.ru>
2019-01-30 16:29:29 +0300

    add execution interface

--- src/Messages/MessageExecutorOptions.php
+++ src/Messages/MessageExecutorOptions.php
@@ -13,7 +13,7 @@ declare(strict_types = 1);
 namespace ServiceBus\Common\Messages;
 
 /**
- * Message execution options
+ * Message execution options marker
  */
 interface MessageExecutorOptions
 {
