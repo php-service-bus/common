35cb525ba583523b8a10a13077ae9e6949e15ae8
desperado <desperado@minsk-info.ru>
2019-02-11 10:28:07 +0300

    change delivery options interface

--- src/Endpoint/DeliveryOptions.php
+++ src/Endpoint/DeliveryOptions.php
@@ -17,5 +17,43 @@ namespace ServiceBus\Common\Endpoint;
  */
 interface DeliveryOptions
 {
+    /**
+     * Create options instance
+     *
+     * @return static
+     */
+    public static function create(): self;
 
+    /**
+     * Apply trace ID
+     *
+     * @param string|int|null $traceId
+     *
+     * @return void
+     */
+    public function withTraceId($traceId): void;
+
+    /**
+     * Apply headers
+     *
+     * @param string                          $key
+     * @param array<string, string|int|float> $value
+     *
+     * @return void
+     */
+    public function withHeader(string $key, $value): void;
+
+    /**
+     * Receive trace id
+     *
+     * @return string|int|float
+     */
+    public function traceId();
+
+    /**
+     * Receive headers
+     *
+     * @return array<string, string|int|float>
+     */
+    public function headers(): array;
 }
