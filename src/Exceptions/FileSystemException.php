089981a58f11731a4247b7b336bf2b4802ddb09d
desperado <desperado@minsk-info.ru>
2019-02-26 18:21:57 +0300

    ignore exception

--- src/Exceptions/FileSystemException.php
+++ src/Exceptions/FileSystemException.php
@@ -18,6 +18,8 @@ namespace ServiceBus\Common\Exceptions;
 final class FileSystemException extends \RuntimeException
 {
     /**
+     * @codeCoverageIgnore
+     *
      * @param string $filePath
      *
      * @return self
