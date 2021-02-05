1fbf6db8175688c8a23c9817f41f3c50c07e20f3
Masiukevich Maksim <contacts@mmasiukevich.dev>
2021-02-05 14:22:10 +0100

    update metadata details

--- src/Context/Metadata.php
+++ src/Context/Metadata.php
@@ -17,25 +17,28 @@ namespace ServiceBus\Common\Context;
  */
 interface Metadata
 {
+    public const SERVICE_BUS_ACTOR_EXTERNAL  = 'external';
+    public const SERVICE_BUS_ACTOR_HANDLER   = 'internalHandler';
+    public const SERVICE_BUS_ACTOR_AGGREGATE = 'internalAggregate';
+    public const SERVICE_BUS_ACTOR_SAGA      = 'internalSaga';
+
     public const SERVICE_BUS_TRACE_ID            = 'X-SERVICE-BUS-TRACE-ID';
     public const SERVICE_BUS_SERIALIZER_TYPE     = 'X-SERVICE-BUS-ENCODER';
     public const SERVICE_BUS_MESSAGE_TYPE        = 'X-SERVICE-BUS-MESSAGE-TYPE';
     public const SERVICE_BUS_MESSAGE_RETRY_COUNT = 'X-SERVICE-BUS-RETRY-COUNT';
+    public const SERVICE_BUS_MESSAGE_ACTOR       = 'X-SERVICE-BUS-ACTOR_TYPE';
 
     /**
      * @psalm-param array<string, string|int|float|bool|null> $variables
      */
-    public static function create(string $messageId, string $traceId, array $variables): static;
+    public static function create(array $variables): static;
 
     /**
-     * Receive incoming message id.
+     * Receive variables
+     *
+     * @psalm-return array<string, string|int|float|bool|null>
      */
-    public function messageId(): string;
-
-    /**
-     * Receive trace message id.
-     */
-    public function traceId(): string;
+    public function variables(): array;
 
     /**
      * Has metadata key.
