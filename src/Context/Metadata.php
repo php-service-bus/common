d56884c9af6b189c815e812bc0fc6d4315dd0788
Masiukevich Maksim <contacts@mmasiukevich.dev>
2021-02-05 15:09:22 +0100

    update metadata details

--- src/Context/Metadata.php
+++ src/Context/Metadata.php
@@ -22,11 +22,25 @@ interface Metadata
     public const SERVICE_BUS_ACTOR_AGGREGATE = 'internalAggregate';
     public const SERVICE_BUS_ACTOR_SAGA      = 'internalSaga';
 
+    public const SERVICE_BUS_INTERNAL_ACTORS = [
+        self::SERVICE_BUS_ACTOR_HANDLER,
+        self::SERVICE_BUS_ACTOR_AGGREGATE,
+        self::SERVICE_BUS_ACTOR_SAGA
+    ];
+
     public const SERVICE_BUS_TRACE_ID            = 'X-SERVICE-BUS-TRACE-ID';
     public const SERVICE_BUS_SERIALIZER_TYPE     = 'X-SERVICE-BUS-ENCODER';
     public const SERVICE_BUS_MESSAGE_TYPE        = 'X-SERVICE-BUS-MESSAGE-TYPE';
     public const SERVICE_BUS_MESSAGE_RETRY_COUNT = 'X-SERVICE-BUS-RETRY-COUNT';
     public const SERVICE_BUS_MESSAGE_ACTOR       = 'X-SERVICE-BUS-ACTOR_TYPE';
+
+    public const SERVICE_BUS_METADATA_KEYS = [
+        self::SERVICE_BUS_TRACE_ID,
+        self::SERVICE_BUS_SERIALIZER_TYPE,
+        self::SERVICE_BUS_MESSAGE_TYPE,
+        self::SERVICE_BUS_MESSAGE_RETRY_COUNT,
+        self::SERVICE_BUS_MESSAGE_ACTOR,
+    ];
 
     /**
      * @psalm-param array<string, string|int|float|bool|null> $variables
@@ -34,7 +48,14 @@ interface Metadata
     public static function create(array $variables): static;
 
     /**
-     * Receive variables
+     * Receive message id.
+     *
+     * @return string
+     */
+    public function messageId(): string;
+
+    /**
+     * Receive variables.
      *
      * @psalm-return array<string, string|int|float|bool|null>
      */
