df79ce49c456b11ba6ed36f45576c7e5148792ee
mmasiukevich <contacts@desperado.dev>
2021-12-05 03:29:44 +0100

    Fix outcome message metadata types

--- src/Context/OutcomeMessageMetadata.php
+++ src/Context/OutcomeMessageMetadata.php
@@ -18,34 +18,43 @@ namespace ServiceBus\Common\Context;
 interface OutcomeMessageMetadata
 {
     /**
+     * @psalm-param non-empty-string $traceId
      * @psalm-param array<string, string|int|float|bool|null> $variables
      */
     public static function create(string $traceId, array $variables = []): self;
 
     /**
      * Add a new key.
+     *
+     * @psalm-param non-empty-string $key
      */
     public function with(string $key, string|int|float|bool|null $value): self;
 
     /**
      * Receive trace id.
+     *
+     * @psalm-return non-empty-string
      */
     public function traceId(): string;
 
     /**
      * Receive variables.
      *
-     * @psalm-return array<string, string|int|float|bool|null>
+     * @psalm-return array<non-empty-string, string|int|float|bool|null>
      */
     public function variables(): array;
 
     /**
      * Has metadata key.
+     *
+     * @psalm-param non-empty-string $key
      */
     public function has(string $key): bool;
 
     /**
      * Receive message metadata value.
+     *
+     * @psalm-param non-empty-string $key
      */
     public function get(string $key, string|int|float|bool|null $default = null): string|int|float|bool|null;
 }
